3e5fd1df3b7699ff0e46f8876dc879d0406de7f0
Danilo Silva <danilo.silva@msn.com>
2021-06-21 11:23:35 -0300

    Correção da Model

--- ASP.NET MVC 5/ProjetoMvcValidacao1/ProjetoMvcValidacao1/Models/Cliente.cs
+++ ASP.NET MVC 5/ProjetoMvcValidacao1/ProjetoMvcValidacao1/Models/Cliente.cs
@@ -2,7 +2,7 @@
 {
     public class Cliente
     {
-        public int ClinteId { get; set; }
+        public int ClienteId { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
