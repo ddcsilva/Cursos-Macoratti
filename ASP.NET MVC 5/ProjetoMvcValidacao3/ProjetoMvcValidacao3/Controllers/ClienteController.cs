ba1ac934887f20f156c6b4d8817a8336c1097414
Danilo Silva <danilo.silva@msn.com>
2021-06-21 22:19:21 -0300

    Validação com Data Annotation Básica

---
+++ ASP.NET MVC 5/ProjetoMvcValidacao3/ProjetoMvcValidacao3/Controllers/ClienteController.cs
@@ -0,0 +1,35 @@
+﻿using ProjetoMvcValidacao3.Models;
+using System.Web.Mvc;
+
+namespace ProjetoMvcValidacao3.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ActionResult Index()
+        {
+            Cliente cliente = new Cliente();
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Cliente cliente)
+        {
+            // A validação falhou?
+            if (!ModelState.IsValid)
+            {
+                // Sim, falhou
+                return View(cliente);
+            }
+            else
+            {
+                // Não, está tudo certo
+                return View("Validacao", cliente);
+            }
+        }
+
+        public ActionResult Validacao(Cliente cliente)
+        {
+            return View(cliente);
+        }
+    }
+}
