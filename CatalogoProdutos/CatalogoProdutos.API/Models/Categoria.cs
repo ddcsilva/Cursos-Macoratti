49a06bfd2383f061ebcc4a9adf9a5a1e6b4f98a1
Danilo Silva <danilo.silva@msn.com>
2021-08-14 18:13:49 -0300

    Criação das Models

---
+++ CatalogoProdutos/CatalogoProdutos.API/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogoProdutos.API.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
---
+++ CatalogoProdutos/CatalogoProdutos.API/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CatalogoProdutos.API.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
