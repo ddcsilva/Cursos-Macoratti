f611cd1c0c581b24d04ce26932be0a7af861137f
Danilo Silva <danilo.silva@msn.com>
2021-08-15 22:44:12 -0300

    Tratamento de Erros com Try/Catch

--- CatalogoProdutos/CatalogoProdutos.API/Controllers/CategoriasController.cs
+++ CatalogoProdutos/CatalogoProdutos.API/Controllers/CategoriasController.cs
@@ -1,7 +1,9 @@
 ﻿using CatalogoProdutos.API.Data;
 using CatalogoProdutos.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -22,53 +24,103 @@ namespace CatalogoProdutos.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Categoria>> Get()
         {
-            return this.context.Categorias.ToList();
+            try
+            {
+                return this.context.Categorias.AsNoTracking().ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao tentar obter as categorias do banco de dados");
+            }
+        }
+
+        // É necessário definir outro endpoint para não ter conflito
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            try
+            {
+                // Com o método Include, eu decido o que incluir na minha lista
+                return this.context.Categorias.AsNoTracking().Include(p => p.Produtos).ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao tentar obter as categorias do banco de dados");
+            }
         }
 
         [HttpGet("{id}", Name = "ObterCategoria")]
         public ActionResult<Categoria> Get(int id)
         {
-            var categoria = this.context.Categorias.AsNoTracking().FirstOrDefault(c => c.Id == id);
+            try
+            {
+                var categoria = this.context.Categorias.AsNoTracking().FirstOrDefault(c => c.Id == id);
 
-            if (categoria == null)
-                return NotFound();
+                if (categoria == null)
+                    return NotFound($"A categoria com id = {id} não foi encontrada");
 
-            return categoria;
+                return categoria;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao tentar obter as categorias do banco de dados");
+            }
         }
 
         [HttpPost]
         public ActionResult<Categoria> Post([FromBody]Categoria categoria)
         {
-            this.context.Add(categoria);
-            this.context.SaveChanges();
+            try
+            {
+                this.context.Add(categoria);
+                this.context.SaveChanges();
 
-            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.Id }, categoria);
+                return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.Id }, categoria);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao tentar criar uma nova categoria");
+            }
         }
 
         [HttpPut("{id}")]
         public ActionResult Put(int id, [FromBody] Categoria categoria)
         {
-            if (id != categoria.Id)
-                return BadRequest();
+            try
+            {
+                if (id != categoria.Id)
+                    return BadRequest($"Não foi possível atualizar a categoria com id = {id}");
 
-            this.context.Entry(categoria).State = EntityState.Modified;
-            this.context.SaveChanges();
+                this.context.Entry(categoria).State = EntityState.Modified;
+                this.context.SaveChanges();
 
-            return Ok();
+                return Ok($"A categoria com id = {id} foi atualizada com sucesso!");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao atualizar a categoria com id = {id}");
+            }
         }
 
         [HttpDelete("{id}")]
         public ActionResult<Categoria> Delete(int id)
         {
-            var categoria = this.context.Categorias.Find(id);
+            try
+            {
+                var categoria = this.context.Categorias.Find(id);
 
-            if (categoria == null)
-                return NotFound();
+                if (categoria == null)
+                    return NotFound($"A categoria com id = {id} não foi encontrada");
 
-            this.context.Categorias.Remove(categoria);
-            this.context.SaveChanges();
+                this.context.Categorias.Remove(categoria);
+                this.context.SaveChanges();
 
-            return categoria;
+                return categoria;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao excluir a categoria com id = {id}");
+            }
         }
     }
 }
