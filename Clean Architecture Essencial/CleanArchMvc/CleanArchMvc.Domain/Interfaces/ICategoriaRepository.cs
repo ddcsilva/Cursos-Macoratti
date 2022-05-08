654b2e340a95d34474ae078c1162d7a4035d4c52
Danilo Silva <danilo.silva@msn.com>
2022-05-08 02:07:02 -0300

    Criando as Interfaces para os Repositórios

---
+++ Clean Architecture Essencial/CleanArchMvc/CleanArchMvc.Domain/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        Task<IEnumerable<Categoria>> GetCategoriasAsync();
+        Task<Categoria> GetCategoriaPorIdAsync(int? id);
+        Task<Categoria> Criar(Categoria categoria);
+        Task<Categoria> Atualizar(Categoria categoria);
+        Task<Categoria> Excluir(Categoria categoria);
+    }
+}
---
+++ Clean Architecture Essencial/CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProdutoRepository
+    {
+        Task<IEnumerable<Produto>> GetProdutosAsync();
+        Task<Produto> GetProdutoPorIdAsync(int? id);
+        Task<Produto> GetProdutoPorCategoriaAsync(int? id);
+        Task<Produto> Criar(Produto produto);
+        Task<Produto> Atualizar(Produto produto);
+        Task<Produto> Excluir(Produto produto);
+    }
+}
