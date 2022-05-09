c132ebc8fde1959c8894aa5811b95dbf4e99a86e
Danilo Silva <danilo.silva@msn.com>
2022-05-09 19:44:44 -0300

    Implementando os Repositórios

---
+++ Clean Architecture Essencial/CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/CategoriaRepository.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private ApplicationDbContext _contexto;
+
+        public CategoriaRepository(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+        public async Task<Categoria> Criar(Categoria categoria)
+        {
+            _contexto.Add(categoria);
+            await _contexto.SaveChangesAsync();
+            return categoria;
+        }
+
+        public async Task<Categoria> Atualizar(Categoria categoria)
+        {
+            _contexto.Update(categoria);
+            await _contexto.SaveChangesAsync();
+            return categoria;
+        }
+
+        public async Task<Categoria> Excluir(Categoria categoria)
+        {
+            _contexto.Remove(categoria);
+            await _contexto.SaveChangesAsync();
+            return categoria;
+        }
+
+        public async Task<Categoria> GetCategoriaPorIdAsync(int? id)
+        {
+            return await _contexto.Categorias.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Categoria>> GetCategoriasAsync()
+        {
+            return await _contexto.Categorias.ToListAsync();
+        }
+    }
+}
---
+++ Clean Architecture Essencial/CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,53 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    internal class ProdutoRepository : IProdutoRepository
+    {
+        private ApplicationDbContext _contexto;
+        public ProdutoRepository(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<Produto> Criar(Produto produto)
+        {
+            _contexto.Add(produto);
+            await _contexto.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<Produto> Atualizar(Produto produto)
+        {
+            _contexto.Update(produto);
+            await _contexto.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<Produto> Excluir(Produto produto)
+        {
+            _contexto.Remove(produto);
+            await _contexto.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<Produto> GetProdutoPorIdAsync(int? id)
+        {
+            return await _contexto.Produtos.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Produto>> GetProdutosAsync()
+        {
+            return await _contexto.Produtos.ToListAsync();
+        }
+
+        public async Task<Produto> GetProdutoPorCategoriaAsync(int? id)
+        {
+            // Eager Loading
+            return await _contexto.Produtos.Include(c => c.Categoria).SingleOrDefaultAsync(p => p.Id == id);
+        }
+    }
+}
