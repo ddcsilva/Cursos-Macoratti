29a4ddec22a7b6693ddb3d3fdaf09ad1458acb18
Danilo Silva <danilo.silva@msn.com>
2022-11-15 10:10:03 -0300

    Criação e Configuração do Contexto (SQL Server LocalDB)

---
+++ ASP_NET_Framework/Criando_Consumindo_Web_Api/WebApiContatos/WebApiContatos/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace WebApiContatos.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("ContatoContext") { }
+
+        public DbSet<Contato> Contatos { get; set; }
+
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
