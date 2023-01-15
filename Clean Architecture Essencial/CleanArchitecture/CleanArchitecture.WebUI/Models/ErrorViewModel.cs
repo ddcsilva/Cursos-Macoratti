ca2af78bb6bf9e78d7798cc66c688e464c1b1064
Danilo Silva <danilo.silva@msn.com>
2023-01-15 01:24:44 -0300

    Criando o projeto WebUI

---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleanArchitecture.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
