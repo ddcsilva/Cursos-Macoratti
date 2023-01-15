36197477376848a8a6c6e073b275a558608d51ef
Danilo Silva <danilo.silva@msn.com>
2023-01-15 13:44:17 -0300

    Definição do Context

---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
