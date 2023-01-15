5d86e4f4b632860adaa9d78c409334823325e34b
Danilo Silva <danilo.silva@msn.com>
2023-01-15 10:33:25 -0300

    Definindo as classes Category e Product como anêmicas

---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        /* EF Relation */
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanArchitecture.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+
+        /* EF Relation */
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
