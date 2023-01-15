ee08653750abb1c101d25ceed3376fa2cd989151
Danilo Silva <danilo.silva@msn.com>
2023-01-15 11:28:10 -0300

    Criando Interfaces do Repositório

---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetByIdAsync(int? id);
+
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+    }
+}
---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> GetProductByCategoryAsync(int? id);
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
