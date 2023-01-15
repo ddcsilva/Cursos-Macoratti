65c00a2ca6056d73202771c60de41ddd28baaa23
Danilo Silva <danilo.silva@msn.com>
2023-01-15 11:28:36 -0300

    Criando o Projeto testes e os testes unitários de domínio

---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,54 @@
+using System;
+using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace CleanArchitecture.Domain.Tests
+{
+    public class CategoryUnitTest1
+    {
+        [Fact(DisplayName = "Create Category With Valid State")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name ");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+        }
+
+        [Fact]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required");
+        }
+
+        [Fact]
+        public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>();
+        }
+
+    }
+}
---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,82 @@
+using System;
+using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace CleanArchitecture.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,
+                99, "product image");
+            action.Should()
+                .NotThrow<Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m,
+                99, "product image");
+
+            action.Should().Throw<Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+
+        [Fact]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99,
+                "product image");
+            action.Should().Throw<Validation.DomainExceptionValidation>()
+                 .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateProduct_LongImageName_DomainExceptionLongImageName()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,
+                99, "product image toooooooooooooooooooooooooooooooooooooooooooo loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg");
+
+            action.Should()
+                .Throw<Validation.DomainExceptionValidation>()
+                 .WithMessage("Invalid image name, too long, maximum 250 characters");
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_WithEmptyImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
+            action.Should().NotThrow<Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_InvalidPriceValue_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", -9.99m,
+                99, "");
+            action.Should().Throw<Validation.DomainExceptionValidation>()
+                 .WithMessage("Invalid price value");
+        }
+
+        [Theory]
+        [InlineData(-5)]
+        public void CreateProduct_InvalidStockValue_ExceptionDomainNegativeValue(int value)
+        {
+            Action action = () => new Product(1, "Pro", "Product Description", 9.99m, value,
+                "product image");
+            action.Should().Throw<Validation.DomainExceptionValidation>()
+                   .WithMessage("Invalid stock value");
+        }
+    }
+}
