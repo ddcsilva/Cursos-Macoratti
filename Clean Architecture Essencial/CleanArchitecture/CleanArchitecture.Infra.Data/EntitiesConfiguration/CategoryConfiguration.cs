68ffcfd08c57dcedb26dd59c8fce9b0a003b8d5c
Danilo Silva <danilo.silva@msn.com>
2023-01-15 13:52:05 -0300

    Configurando as entidades usando a Fluent API

---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+              new Category(1, "Material Escolar"),
+              new Category(2, "Eletrônicos"),
+               new Category(3, "Acessórios")
+            );
+        }
+    }
+}
---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.HasOne(e => e.Category).WithMany(e => e.Products)
+                .HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
