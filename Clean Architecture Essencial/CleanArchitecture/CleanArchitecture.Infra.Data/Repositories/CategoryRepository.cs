a0a08ded3bdb18e5c960fea8a4f1d123cea4ab58
Danilo Silva <danilo.silva@msn.com>
2023-01-15 22:07:58 -0300

    Implementação do Repositório Concreto de Category

---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
