1ec34dd3a7799c714b0314381c4c654a968651f0
Danilo Silva <danilo.silva@msn.com>
2023-01-15 22:07:28 -0300

    Alteração do nome do método

--- Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Domain/Interfaces/ICategoryRepository.cs
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Domain/Interfaces/ICategoryRepository.cs
@@ -6,7 +6,7 @@ namespace CleanArchitecture.Domain.Interfaces
 {
     public interface ICategoryRepository
     {
-        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<IEnumerable<Category>> GetAllAsync();
         Task<Category> GetByIdAsync(int? id);
 
         Task<Category> CreateAsync(Category category);
