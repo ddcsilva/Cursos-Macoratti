e0ac6fec07eb6110e18bdc80fa4cd1460b86c29c
Danilo Silva <danilo.silva@msn.com>
2023-01-16 21:48:43 -0300

    Definindo as configurações de Injeção de Dependência

---
+++ Clean Architecture Essencial/CleanArchitecture/CleanArchitecture.Infra.IoC/DependecyInjection.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infra.Data.Context;
+using CleanArchitecture.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
